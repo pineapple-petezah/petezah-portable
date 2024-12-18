6311e2477b0fc92fda191666f54e19560ac1c94b
pineapple-petezah <petezahindustries@gmail.com>
2024-12-18 16:27:57 -0500

    Create index.js

---
+++ storage/js/index.js
@@ -0,0 +1,18 @@
+/* Scroll Behavior */
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('header');
+  if (window.scrollY > 50) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
+
+/* FAQ Interaction (JavaScript) */
+const faqItems = document.querySelectorAll('.faq-item');
+faqItems.forEach(item => {
+  const question = item.querySelector('.faq-question');
+  question.addEventListener('click', () => {
+    item.classList.toggle('active');
+  });
+});
